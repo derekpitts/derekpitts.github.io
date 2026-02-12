d7a688545e0fb1b26c1cf6b05b7f77e521260dfe
Derek <dspitts10@gmail.com>
2026-02-12 13:37:09 -0700

    Update Flutter app to version 1.0.7, change service worker version, and refactor method names in main.dart.js

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3065260318" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2005005821" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
